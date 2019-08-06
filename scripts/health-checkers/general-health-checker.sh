6c1fc0863dccea31f96564fe8dbbbbaa44e853b7
danielsilverio-dss <dss.123@hotmail.com>
2019-08-06 14:17:01 -0300

    Update scripts

---
+++ scripts/health-checkers/general-health-checker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Memória: "
+free -m
+echo "\n\n\n"
+echo "Espaço em disco: "
+df -h
--- scripts/run-squirrel.sh
+++ scripts/run-squirrel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd ~/dev/squirrel-sql-3.9.1 \
+cd ~/dev/dev-storage/squirrel-sql-3.9.1 \
 && java -jar squirrel-sql.jar
